4707bd74687983e2d78f1002e486690efcd05579
Hanif Jundana <142137114+JunHanif@users.noreply.github.com>
2026-09-23 12:14:31 +0700

    fix playGame 5 times

--- javascript.js
+++ javascript.js
@@ -1,20 +1,14 @@
-let humanScore = 0;
-let computerScore = 0;
-
-
 function getComputerChoice() {
-    const computerChoice =  Math.floor(Math.random() * 3) + 1;
+    const computerChoice = Math.floor(Math.random() * 3) + 1;
 
     switch (computerChoice) {
-        case 1 :{
+        case 1: {
             return "rock"
-            break
         }
-        case 2 :{
+        case 2: {
             return "paper"
-            break
         }
-        case 3 : {
+        case 3: {
             return "scissor"
         }
     }
@@ -22,22 +16,49 @@ function getComputerChoice() {
 
 
 function getHumanChoice() {
-    const humanChoice = prompt("Enter your choice: (rock, paper, scissor");
-    return humanChoice
+    const humanChoice = prompt("Enter your choice: (rock, paper, scissor)");
+    return humanChoice ? humanChoice.toLowerCase() : "";
 }
 
+function printScore(humanScore, computerScore) {
+    console.log("score: ");
+    console.log("human score: " + humanScore);
+    console.log("computer score: " + computerScore);
+}
+
+
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("you win")
-        humanScore += 1;
-        console.log("your score: " + humanScore);
-        console.log("computer score: " + computerScoreScore)
+    if (humanChoice === computerChoice) {
+        console.log("draw, " + humanChoice + " = " + computerChoice)
+        return 0;
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissor") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissor" && computerChoice === "paper")
+    ) {
+        console.log("you win, " + humanChoice + " beats " + computerChoice);
+        return 1
+    } else {
+        console.log("You lose, " + computerChoice + " beats " + humanChoice)
+        return -1
     }
 }
 
-const humanChoice = getHumanChoice().toLowerCase();
-const computerChoice = getComputerChoice();
+function playGame() {
 
-console.log(humanChoice);
-console.log(computerChoice);
-playRound(humanChoice, computerChoice);
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        let score = playRound(getHumanChoice(), getComputerChoice());
+        switch (score) {
+            case 1: humanScore++; break;
+            case -1: computerScore++; break
+        }
+        printScore(humanScore, computerScore)
+    }
+}
+
+
+
+playGame()
